b89dc1b015ee89cb199ca31b785576e2378d3624
Reinis Starks <reinis.kromulis@ivendi.com>
2025-05-18 16:59:58 +0300

    bug: nested button button is not allowed

--- src/lib/components/Panel/bottom-nav-link.svelte
+++ src/lib/components/Panel/bottom-nav-link.svelte
@@ -5,7 +5,6 @@
 	let {  class: className, text }: { text?: string; class?: string; href?: string } = $props();
 </script>
 
-<button>
 	<div class="relative">
 		<img src={panel} alt="feats" class={twMerge('', className)} />
 		<div
@@ -14,4 +13,3 @@
 			{text}
 		</div>
 	</div>
-</button>
