03bed0b9ceb4aae434067813a06aa9f529a02621
Reinis Starks <reinis.kromulis@ivendi.com>
2025-06-30 22:31:31 +0300

    test, stop invalidating images each build

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss()],
+  plugins: [sveltekit(), tailwindcss()],
   build: {
-    assetsInlineLimit: 0
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name][extname]', // stop invalidating assets each build
+      },
+    }
   }
 });
