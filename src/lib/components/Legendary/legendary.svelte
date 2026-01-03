e18ee9bf20819689df8947f89cd123551f3e9180
Reinis Starks <reinis.kromulis@ivendi.com>
2026-01-04 01:33:14 +0200

    display owned, planned arrows conditionally

--- src/lib/components/Legendary/legendary.svelte
+++ src/lib/components/Legendary/legendary.svelte
@@ -10,17 +10,17 @@
 	<img src={legendMap[legend]} alt={legend} class="m-auto h-20" />
 	<div class="capitalize">{legend}</div>
 	<div class="counter flex flex-col gap-4">
-		<div class="flex gap-4">
-			<NavArrow class="size-5" dir="left" onclick={() => shards.dec(legend)} />
-			<div aria-label="count" class="">{shards.legendaryCount[legend]}</div>
-			<NavArrow class="size-5" onclick={() => shards.inc(legend)} />
-		</div>
-
 		{#if enableOwned}
 			<div class="flex gap-4">
 				<NavArrow class="size-5 invert-100" dir="left" onclick={() => cost.dec(legend)} />
 				<div aria-label="count" class="">{cost.legendaryCount[legend]}</div>
 				<NavArrow class="size-5 invert-100" onclick={() => cost.inc(legend)} />
+			</div>
+		{:else}
+			<div class="flex gap-4">
+				<NavArrow class="size-5" dir="left" onclick={() => shards.dec(legend)} />
+				<div aria-label="count" class="">{shards.legendaryCount[legend]}</div>
+				<NavArrow class="size-5" onclick={() => shards.inc(legend)} />
 			</div>
 		{/if}
 	</div>
