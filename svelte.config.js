2544a0fc3478bf5c15667aabc45a567f98695b16
Reinis Starks <reinis.kromulis@ivendi.com>
2026-01-05 21:20:52 +0200

    fix build error

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,13 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({ edge: false, split: false }),
-		paths: {
-			base: '/sv-necromerger-helper'
-		}
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({ edge: false, split: false }),
+    paths: {
+      base: '/sv-necromerger-helper'
+    }
+  }
 };
 
 export default config;
