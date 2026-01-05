1a802759523bcc9d88844b8a091c983749f76243
Reinis Starks <reinis.kromulis@ivendi.com>
2026-01-05 21:13:03 +0200

    fix: asset paths

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({ edge: false, split: false }) }
+	kit: {
+		adapter: adapter({ edge: false, split: false }),
+		paths: {
+			base: '/sv-necromerger-helper'
+		}
+	}
 };
 
 export default config;
